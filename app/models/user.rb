5eb252dc839af7579aee20b8f2f63c2312744cf9
Wallison de Oliveira <wallison_93@hotmail.com>
2016-08-22 21:02:24 -0300

    First commit. Added secure password and User

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
+end
