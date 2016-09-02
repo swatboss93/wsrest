39f2e87a83c9da30dbbe68fe7e0e41948e04db6f
Wallison de Oliveira <wallison_93@hotmail.com>
2016-09-02 11:22:47 -0300

    Create a user correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
+    @user.password = params[:password]
     if @user.save
       render json: @user, status: :created, location: @user
     else
