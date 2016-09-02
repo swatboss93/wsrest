49c6a4949e43082e09bbd453e53f23d28618960b
Wallison de Oliveira <wallison_93@hotmail.com>
2016-09-02 17:28:02 -0300

    Username column deleted from User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829175341) do
+ActiveRecord::Schema.define(version: 20160902202620) do
 
   create_table "pictures", force: :cascade do |t|
     t.integer  "user_id"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20160829175341) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "username"
     t.string   "snapchat"
     t.string   "whatsapp"
     t.string   "facebook"
