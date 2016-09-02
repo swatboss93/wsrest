b0b681a1944a54b81eac5c4aa44aeaedecd6b68b
Wallison de Oliveira <wallison_93@hotmail.com>
2016-09-02 11:41:14 -0300

    Serialize email instead of username

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :snapchat, :whatsapp, :facebook, :instagram
+  attributes :id, :name, :email, :snapchat, :whatsapp, :facebook, :instagram
   has_many :pictures
 end
